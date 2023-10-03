eefccfb8f046f0811b2f82031defdc7a42407003
batoulll-ghosn <batoul.ghosn@isae.edu.lb>
2023-10-03 12:59:24 +0300

    done with whyus

--- src/whyus.js
+++ src/whyus.js
@@ -1,63 +1,117 @@
-import React, { useState } from 'react';
-import Circle from './circle.jpg';
+import React, { useState, useEffect } from 'react';
 import './index.css';
 
-const WhyChooseUs = () => {
+const App = () => {
+    const [data, setData] = useState({});
     const [isEditing, setIsEditing] = useState(false);
-    const [editedText, setEditedText] = useState(
-        "Choose the Translators Union for unparalleled linguistic expertise. Our dedicated community of skilled translators covers 20+ languages, ensuring precision and cultural sensitivity. Join us for professional, integrity-driven translations that elevate your content."
-    );
-    const [editedValues, setEditedValues] = useState([
-        "Multilingual Mastery: Our union houses experts fluent in 20+ languages, guaranteeing precise translations across diverse linguistic needs.",
-        "Community Collaboration: Join our close-knit community for collaborative knowledge-sharing, ensuring top-notch translation quality.",
-        "Unwavering Excellence: We uphold professionalism, integrity, and linguistic mastery, offering a trusted resource for your translation requirements."
-    ]);
+    const [editedData, setEditedData] = useState({});
+    const [isLoading, setIsLoading] = useState(true);
+    const [imageUrl, setImageUrl] = useState('');
+    const [file, setFile] = useState(null);
+
+    // State variables for the last 3 text values
+    const [editedFirstTick, setEditedFirstTick] = useState('');
+    const [editedSecondTick, setEditedSecondTick] = useState('');
+    const [editedThirdTick, setEditedThirdTick] = useState('');
+
+    useEffect(() => {
+        // Fetch data from the server when the component mounts
+        async function fetchData() {
+            try {
+                const response = await fetch('http://localhost:5000/whyus/kel'); // Replace with your API endpoint
+                if (response.ok) {
+                    const result = await response.json();
+                    if (result.data && result.data.length > 0) {
+                        setData(result.data[0]);
+                        setEditedData(result.data[0]);
+                    }
+                    setIsLoading(false);
+                } else {
+                    console.error('Failed to fetch data from the server.');
+                }
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        }
+        fetchData();
+    }, []);
 
     const handleEditClick = () => {
         setIsEditing(!isEditing);
     };
 
-    const handleTextChange = (e) => {
-        setEditedText(e.target.value);
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setEditedData({ ...editedData, [name]: value });
     };
 
-    const handleValueChange = (e, index) => {
-        const updatedValues = [...editedValues];
-        updatedValues[index] = e.target.value;
-        setEditedValues(updatedValues);
-    };
-
-    const handleDeleteValue = (index) => {
-        const updatedValues = [...editedValues];
-        updatedValues.splice(index, 1);
-        setEditedValues(updatedValues);
-    };
-
-    const saveToDatabase = async () => {
+    const handleSaveClick = async () => {
+        // Send edited data back to the server when saving
         try {
-            const schema = {
-                whyus: editedText,
-                firsttick: editedValues[0],
-                secondtick: editedValues[1],
-                thirdtick: editedValues[2],
-            };
-            console.log(schema);
-            console.log(JSON.stringify(schema));
-            const response = await fetch('http://localhost:5000/batoul/', {
-                method: 'POST',
+            const response = await fetch(`http://localhost:5000/whyus/update/${data._id}`, {
+                method: 'PUT', // Assuming you use PUT for updates
                 headers: {
                     'Content-Type': 'application/json',
                 },
-                body: JSON.stringify(schema),
+                body: JSON.stringify(editedData),
             });
-            console.log(response);
+
             if (response.ok) {
-                console.log('Data saved successfully.');
+                console.log('Data updated successfully.');
+
+                // Update the document in MongoDB with the same ID
+                const updateResponse = await fetch(`http://localhost:5000/whyus/updateDocument/${data._id}`, {
+                    method: 'PUT', // Assuming you use PUT for updates
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(editedData),
+                });
+
+                if (updateResponse.ok) {
+                    console.log(`Document with ID ${data._id} updated in MongoDB.`);
+                } else {
+                    console.error('Failed to update document in MongoDB.');
+                }
+
+                setData(editedData); // Update displayed data
             } else {
-                console.error('Failed to save data to the database.');
+                console.error('Failed to update data on the server.');
             }
         } catch (error) {
-            console.error('Error saving data:', error);
+            console.error('Error updating data:', error);
+        }
+        setIsEditing(false); // Exit edit mode
+    };
+
+    const handleFileChange = (e) => {
+        setFile(e.target.files[0]);
+    };
+
+    const handleImageUpload = async () => {
+        if (!file) {
+            console.error('No file selected.');
+            return;
+        }
+
+        const formData = new FormData();
+        formData.append('file', file);
+
+        try {
+            const response = await fetch('http://localhost:8000/upload', {
+                method: 'POST',
+                body: formData,
+            });
+
+            if (response.ok) {
+                const data = await response.json();
+                const uploadedImageUrl = data.imageUrl;
+                setEditedData({ ...editedData, image: uploadedImageUrl });
+            } else {
+                console.error('Error uploading the file.');
+            }
+        } catch (error) {
+            console.error('Error:', error);
         }
     };
 
@@ -65,47 +119,133 @@ const WhyChooseUs = () => {
         <div>
             <h2>Why Choose Us?</h2>
             {isEditing ? (
-                <textarea value={editedText} onChange={handleTextChange} />
+                <>
+                    <textarea
+                        name="whyus"
+                        value={editedData.whyus}
+                        onChange={handleInputChange}
+                    />
+                    {/* Input fields for the last 3 text values */}
+                    <input
+                        type="text"
+                        name="firsttick"
+                        value={editedData.firsttick}
+                        onChange={handleInputChange}
+                    />
+                    <input
+                        type="text"
+                        name="secondtick"
+                        value={editedData.secondtick}
+                        onChange={handleInputChange}
+                    />
+                    <input
+                        type="text"
+                        name="thirdtick"
+                        value={editedData.thirdtick}
+                        onChange={handleInputChange}
+                    />
+                    <input
+                        type="text"
+                        name="image"
+                        placeholder="Image URL"
+                        value={editedData.image}
+                        onChange={handleInputChange}
+                    />
+                </>
             ) : (
                 <div style={{ display: 'flex', alignItems: 'center' }}>
                     <div style={{ width: '50%' }}>
-                        <p>{editedText}</p>
+                        <p>{data.whyus}</p>
+                        <p><img className='' src='' />{data.sfirsttick}</p>
+                        <p><img className='' src='' />{data.ssecondtick}</p>
+                        <p><img className='' src='' />{data.sthirdtick}</p>
                     </div>
+                    <img style={{ width: '150px', height: '150px' }} src={data.image} alt="Image" />
                 </div>
             )}
+
             <div className="content-why">
                 <div className="list-why">
-                    {editedValues.map((value, index) => (
-                        <div key={index} className="list-item">
-                            {isEditing ? (
-                                <>
-                                    <input
-                                        type="text"
-                                        value={value}
-                                        onChange={(e) => handleValueChange(e, index)}
-                                    />
-                                    <button onClick={() => handleDeleteValue(index)}>Delete</button>
-                                </>
-                            ) : (
-                                <>
-                                    <p>{value}</p>
-                                </>
-                            )}
-                        </div>
-                    ))}
-                </div>
-                <div className="right-why">
-                    <img className="right-circle" src={Circle} alt="Circle" />
+                    <div className="list-item">
+                        {isEditing ? (
+                            <>
+                                <input
+                                    type="text"
+                                    name="firsttick"
+                                    value={editedData.firsttick}
+                                    onChange={handleInputChange}
+                                />
+                            </>
+                        ) : (
+                            <>
+                                <img className='' src='' />
+                                <p>{data.firsttick}</p>
+                            </>
+                        )}
+                    </div>
+                    <div className="list-item">
+                        {isEditing ? (
+                            <>
+                                <input
+                                    type="text"
+                                    name="secondtick"
+                                    value={editedData.secondtick}
+                                    onChange={handleInputChange}
+                                />
+                            </>
+                        ) : (
+                            <>
+                                <img className='' src='' />
+                                <p>{data.secondtick}</p>
+                            </>
+                        )}
+                    </div>
+                    <div className="list-item">
+                        {isEditing ? (
+                            <>
+                                <input
+                                    type="text"
+                                    name="thirdtick"
+                                    value={editedData.thirdtick}
+                                    onChange={handleInputChange}
+                                />
+                            </>
+                        ) : (
+                            <>
+                                <img className='' src='' />
+                                <p>{data.thirdtick}</p>
+                            </>
+                        )}
+                    </div>
                 </div>
             </div>
-            <button onClick={handleEditClick}>
-                {isEditing ? 'Save' : 'Edit'}
-            </button>
+            <div style={{ display: 'flex' }}>
+                <button onClick={handleEditClick}>
+                    {isEditing ? 'Cancel' : 'Edit'}
+                </button>
+                {isEditing && (
+                    <button onClick={handleSaveClick}>Save</button>
+                )}
+            </div>
+
             {isEditing && (
-                <button onClick={saveToDatabase}>Save to Database</button>
+                <>
+                    {/* Image upload form */}
+                    <h2>Upload an Image</h2>
+                    <form encType="multipart/form-data">
+                        <input type="file" onChange={handleFileChange} />
+                        <button type="button" onClick={handleImageUpload}>Upload</button>
+                    </form>
+
+                    {imageUrl && (
+                        <div>
+                            Image URL: <a href={imageUrl} target="_blank" rel="noopener noreferrer" style={{ color: 'blue' }}>{imageUrl}</a>
+                        </div>
+                    )}
+                </>
             )}
         </div>
     );
 };
 
-export default WhyChooseUs;
+export default App;
